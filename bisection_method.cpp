7b38c1a69264654e1fcda9b4ba8bbdbc8997c284
Diego López <user@Diegos-MacBook-Pro-3.local>
2022-10-20 14:12:07 -0400

    first commit

---
+++ bisection_method.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+This program uses the bisection method to approximate the value of R, where are is 
+the resistance on a circuit such that the charge on that circuit dissipates 1% of its
+initial value in 0.05 seconds. That is to say q(0.05)/q0 = 0.01. L = 5H and C = 10^-4F.
+*/
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
